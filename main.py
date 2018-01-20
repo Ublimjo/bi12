dfb7151a6e3cfb3300f739b1dbb66f32ebb4cca4
Ublim <ublimjo@gmail.com>
2018-01-20 23:08:02 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/env python
+import argparse
+import sys
+
+version = 1.0
+
+def main():
+	print("===========================================")
+	print("  Baiboly fandikan-teny ny tontolo vaovao")
+	print("    Version :", version)
+	print("===========================================")
+	
+
+if len(sys.argv) == 1:
+	main()
+else:
+	view()
