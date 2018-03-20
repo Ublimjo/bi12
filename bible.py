627133b0f43c080d2af46b8ef2e83912fa73e745
Ublim <ublimjo@gmail.com>
2018-03-20 22:44:30 +0300

    reformatting code with black

--- bible.py
+++ bible.py
@@ -1,81 +1,149 @@
 import edit_distance as ed
-bookList = ["genesisy", "eksodosy", "levitikosy", "nomery", "deoteronomia", "josoa", "mpitsara", "rota", "1 samoela", "2 samoela", "1 mpanjaka", "2 mpanjaka", "1 tantara", "2 tantara", "ezra", "nehemia", "estera", "joba", "salamo", "ohabolana", "mpitoriteny", "tononkiran’i solomona", "isaia", "jeremia", "fitomaniana", "ezekiela", "daniela", "hosea", "joela", "amosa", "obadia", "jona", "mika", "nahoma", "habakoka", "zefania", "hagay", "zakaria", "malakia", "matio", "marka", "lioka", "jaona", "asan’ny apostoly", "romanina", "1 korintianina", "2 korintianina", "galatianina", "efesianina", "filipianina", "kolosianina", "1 tesalonianina", "2 tesalonianina", "1 timoty", "2 timoty", "titosy", "filemona", "hebreo", "jakoba", "1 petera", "2 petera", "1 jaona", "2 jaona", "3 jaona", "joda", "apokalypsy"]
+
+bookList = [
+    "genesisy",
+    "eksodosy",
+    "levitikosy",
+    "nomery",
+    "deoteronomia",
+    "josoa",
+    "mpitsara",
+    "rota",
+    "1 samoela",
+    "2 samoela",
+    "1 mpanjaka",
+    "2 mpanjaka",
+    "1 tantara",
+    "2 tantara",
+    "ezra",
+    "nehemia",
+    "estera",
+    "joba",
+    "salamo",
+    "ohabolana",
+    "mpitoriteny",
+    "tononkiran’i solomona",
+    "isaia",
+    "jeremia",
+    "fitomaniana",
+    "ezekiela",
+    "daniela",
+    "hosea",
+    "joela",
+    "amosa",
+    "obadia",
+    "jona",
+    "mika",
+    "nahoma",
+    "habakoka",
+    "zefania",
+    "hagay",
+    "zakaria",
+    "malakia",
+    "matio",
+    "marka",
+    "lioka",
+    "jaona",
+    "asan’ny apostoly",
+    "romanina",
+    "1 korintianina",
+    "2 korintianina",
+    "galatianina",
+    "efesianina",
+    "filipianina",
+    "kolosianina",
+    "1 tesalonianina",
+    "2 tesalonianina",
+    "1 timoty",
+    "2 timoty",
+    "titosy",
+    "filemona",
+    "hebreo",
+    "jakoba",
+    "1 petera",
+    "2 petera",
+    "1 jaona",
+    "2 jaona",
+    "3 jaona",
+    "joda",
+    "apokalypsy",
+]
 
 
 def parseInt(enter):
-	try:
-		return int(enter)
-	except:
-		return 0
+    try:
+        return int(enter)
+
+    except:
+        return 0
+
 
 def getDist(enter):
-	"""
+    """
 		Function to test shortest distance of string
 		Mitio -> Matio
 		:param enter: string to test
 	"""
-	solver = ed.EditDistance()
-	leaf = 0
-	mean = []
-	for book in bookList:
-		mean.append(solver.solve(enter, book))
-	while True:
-		try:
-			return [leaf, bookList[mean.index(leaf)]]
-		except ValueError:
-			leaf += 1
+    solver = ed.EditDistance()
+    leaf = 0
+    mean = []
+    for book in bookList:
+        mean.append(solver.solve(enter, book))
+    while True:
+        try:
+            return [leaf, bookList[mean.index(leaf)]]
+
+        except ValueError:
+            leaf += 1
+
 
 class bibleParse:
-	def __init__(self):
-		self.book = ""
-		self.raw_verset = ""
-		self.verset1 = 0
-		self.raw_verset2 = ""
-		self.verset2 = []
 
-	def parse(self, enter):
-		"""
+    def __init__(self):
+        self.book = ""
+        self.raw_verset = ""
+        self.verset1 = 0
+        self.raw_verset2 = ""
+        self.verset2 = []
+
+    def parse(self, enter):
+        """
 			parse verset input (Book with verset)
 			:param enter: verset to parse
 				Matio 24:14,16-20 -> Matio 24:14,16,17,18,19,20
 		"""
-		cmd = enter.lower().split(" ")
-		if len(cmd) == 1:
-			return False
+        cmd = enter.lower().split(" ")
+        if len(cmd) == 1:
+            return False
 
-		prebook1 = (cmd[0] + " " + cmd[1])
-		prebook2 = (cmd[0])
-
-		if getDist(prebook2)[0] < getDist(prebook1)[0]:
-				entbook = getDist(prebook2)[1]
-				withSub = False
-		elif getDist(prebook1)[0] < getDist(prebook2)[0]:
-				entbook = getDist(prebook1)[1]
-				withSub = True
-	# -- test verset
-		if not withSub:
-			verset = cmd[1].split(":")[0]
-			subverset = cmd[1].split(":")[1].split(",")
-		else:
-			verset = cmd[2].split(":")[0]
-			subverset = cmd[2].split(":")[1].split(",")
-		
-		# convert "Matio 24:14-17" to "Matio 24:14,15,16,17"
-		for i, lsvt in enumerate(subverset):
-			if "-" in str(lsvt):
-				del subverset[i]
-				a = int(lsvt.split("-")[0])
-				z = int(lsvt.split("-")[1])
-				while a <= z:
-					subverset.insert(i, a)
-					a += 1
-					i += 1
-		
-	#activate
-		self.book = entbook
-		self.raw_verset = verset+":"+str(subverset)
-		self.verset1 = verset
-		self.raw_verset2 = subverset
-		self.verset2 = subverset
-		
-		
+        prebook1 = (cmd[0] + " " + cmd[1])
+        prebook2 = (cmd[0])
+        if getDist(prebook2)[0] < getDist(prebook1)[0]:
+            entbook = getDist(prebook2)[1]
+            withSub = False
+        elif getDist(prebook1)[0] < getDist(prebook2)[0]:
+            entbook = getDist(prebook1)[1]
+            withSub = True
+        # -- test verset
+        if not withSub:
+            verset = cmd[1].split(":")[0]
+            subverset = cmd[1].split(":")[1].split(",")
+        else:
+            verset = cmd[2].split(":")[0]
+            subverset = cmd[2].split(":")[1].split(",")
+        # convert "Matio 24:14-17" to "Matio 24:14,15,16,17"
+        for i, lsvt in enumerate(subverset):
+            if "-" in str(lsvt):
+                del subverset[i]
+                a = int(lsvt.split("-")[0])
+                z = int(lsvt.split("-")[1])
+                while a <= z:
+                    subverset.insert(i, a)
+                    a += 1
+                    i += 1
+        # activate
+        self.book = entbook
+        self.raw_verset = verset + ":" + str(subverset)
+        self.verset1 = verset
+        self.raw_verset2 = subverset
+        self.verset2 = subverset
--- part.py
+++ part.py
@@ -1,79 +1,81 @@
 import bible
 from bs4 import BeautifulSoup
 
-bookList = [["genesisy",              "006_B12_.GE"],
-            ["eksodosy",              "007_B12_.EX"],
-            ["levitikosy",            "008_B12_.LE"],
-            ["nomery",                "009_B12_.NU"],
-            ["deoteronomia",          "010_B12_.DE"],
-            ["josoa",                 "011_B12_.JOS"],
-            ["mpitsara",              "012_B12_.JG"],
-            ["rota",                  "013_B12_.RU"],
-            ["1 samoela",             "014_B12_.1SA"],
-            ["2 samoela",             "015_B12_.2SA"],
-            ["1 mpanjaka",            "016_B12_.1KI"],
-            ["2 mpanjaka",            "017_B12_.2KI"],
-            ["1 tantara",             "018_B12_.1CH"],
-            ["2 tantara",             "019_B12_.2CH"],
-            ["ezra",                  "020_B12_.EZR"],
-            ["nehemia",               "021_B12_.NE"],
-            ["estera",                "022_B12_.ES"],
-            ["joba",                  "023_B12_.JOB"],
-            ["salamo",                "024_B12_.PS"],
-            ["ohabolana",             "025_B12_.PR"],
-            ["mpitoriteny",           "026_B12_.EC"],
-            ["tononkiran’i solomona", "027_B12_.CA"],
-            ["isaia",                 "028_B12_.ISA"],
-            ["jeremia",               "029_B12_.JER"],
-            ["fitomaniana",           "030_B12_.LA"],
-            ["ezekiela",              "031_B12_.EZE"],
-            ["daniela",               "032_B12_.DA"],
-            ["hosea",                 "033_B12_.HOS"],
-            ["joela",                 "034_B12_.JOE"],
-            ["amosa",                 "035_B12_.AM"],
-            ["obadia",                "036_B12_.OB"],
-            ["jona",                  "037_B12_.JON"],
-            ["mika",                  "038_B12_.MIC"],
-            ["nahoma",                "039_B12_.NAH"],
-            ["habakoka",              "040_B12_.HAB"],
-            ["zefania",               "041_B12_.ZEP"],
-            ["hagay",                 "042_B12_.HAG"],
-            ["zakaria",               "043_B12_.ZEC"],
-            ["malakia",               "044_B12_.MAL"],
-            ["matio",                 "045_B12_.MT"],
-            ["marka",                 "046_B12_.MR"],
-            ["lioka",                 "047_B12_.LU"],
-            ["jaona",                 "048_B12_.JOH"],
-            ["asan’ny apostoly",      "049_B12_.AC"],
-            ["romanina",              "050_B12_.RO"],
-            ["1 korintianina",        "051_B12_.1CO"],
-            ["2 korintianina",        "052_B12_.2CO"],
-            ["galatianina",           "053_B12_.GA"],
-            ["efesianina",            "054_B12_.EPH"],
-            ["filipianina",           "055_B12_.PHP"],
-            ["kolosianina",           "056_B12_.COL"],
-            ["1 tesalonianina",       "057_B12_.1TH"],
-            ["2 tesalonianina",       "058_B12_.2TH"],
-            ["1 timoty",              "059_B12_.1TI"],
-            ["2 timoty",              "060_B12_.2TI"],
-            ["titosy",                "061_B12_.TIT"],
-            ["filemona",              "062_B12_.PHM"],
-            ["hebreo",                "063_B12_.HEB"],
-            ["jakoba",                "064_B12_.JAS"],
-            ["1 petera",              "065_B12_.1PE"],
-            ["2 petera",              "066_B12_.2PE"],
-            ["1 jaona",               "067_B12_.1JO"],
-            ["2 jaona",               "068_B12_.2JO"],
-            ["3 jaona",               "069_B12_.3JO"],
-            ["joda",                  "070_B12_.JUD"],
-            ["apokalypsy",            "071_B12_.RE"]]
+bookList = [
+    ["genesisy", "006_B12_.GE"],
+    ["eksodosy", "007_B12_.EX"],
+    ["levitikosy", "008_B12_.LE"],
+    ["nomery", "009_B12_.NU"],
+    ["deoteronomia", "010_B12_.DE"],
+    ["josoa", "011_B12_.JOS"],
+    ["mpitsara", "012_B12_.JG"],
+    ["rota", "013_B12_.RU"],
+    ["1 samoela", "014_B12_.1SA"],
+    ["2 samoela", "015_B12_.2SA"],
+    ["1 mpanjaka", "016_B12_.1KI"],
+    ["2 mpanjaka", "017_B12_.2KI"],
+    ["1 tantara", "018_B12_.1CH"],
+    ["2 tantara", "019_B12_.2CH"],
+    ["ezra", "020_B12_.EZR"],
+    ["nehemia", "021_B12_.NE"],
+    ["estera", "022_B12_.ES"],
+    ["joba", "023_B12_.JOB"],
+    ["salamo", "024_B12_.PS"],
+    ["ohabolana", "025_B12_.PR"],
+    ["mpitoriteny", "026_B12_.EC"],
+    ["tononkiran’i solomona", "027_B12_.CA"],
+    ["isaia", "028_B12_.ISA"],
+    ["jeremia", "029_B12_.JER"],
+    ["fitomaniana", "030_B12_.LA"],
+    ["ezekiela", "031_B12_.EZE"],
+    ["daniela", "032_B12_.DA"],
+    ["hosea", "033_B12_.HOS"],
+    ["joela", "034_B12_.JOE"],
+    ["amosa", "035_B12_.AM"],
+    ["obadia", "036_B12_.OB"],
+    ["jona", "037_B12_.JON"],
+    ["mika", "038_B12_.MIC"],
+    ["nahoma", "039_B12_.NAH"],
+    ["habakoka", "040_B12_.HAB"],
+    ["zefania", "041_B12_.ZEP"],
+    ["hagay", "042_B12_.HAG"],
+    ["zakaria", "043_B12_.ZEC"],
+    ["malakia", "044_B12_.MAL"],
+    ["matio", "045_B12_.MT"],
+    ["marka", "046_B12_.MR"],
+    ["lioka", "047_B12_.LU"],
+    ["jaona", "048_B12_.JOH"],
+    ["asan’ny apostoly", "049_B12_.AC"],
+    ["romanina", "050_B12_.RO"],
+    ["1 korintianina", "051_B12_.1CO"],
+    ["2 korintianina", "052_B12_.2CO"],
+    ["galatianina", "053_B12_.GA"],
+    ["efesianina", "054_B12_.EPH"],
+    ["filipianina", "055_B12_.PHP"],
+    ["kolosianina", "056_B12_.COL"],
+    ["1 tesalonianina", "057_B12_.1TH"],
+    ["2 tesalonianina", "058_B12_.2TH"],
+    ["1 timoty", "059_B12_.1TI"],
+    ["2 timoty", "060_B12_.2TI"],
+    ["titosy", "061_B12_.TIT"],
+    ["filemona", "062_B12_.PHM"],
+    ["hebreo", "063_B12_.HEB"],
+    ["jakoba", "064_B12_.JAS"],
+    ["1 petera", "065_B12_.1PE"],
+    ["2 petera", "066_B12_.2PE"],
+    ["1 jaona", "067_B12_.1JO"],
+    ["2 jaona", "068_B12_.2JO"],
+    ["3 jaona", "069_B12_.3JO"],
+    ["joda", "070_B12_.JUD"],
+    ["apokalypsy", "071_B12_.RE"],
+]
 
-#test = bible.bibleParse()
+
+# test = bible.bibleParse()
 #test.parse("1 Jaona 5:19")
 #print("Book :", test.book)
 #print("Verset :", test.verset1)
 #print("Subverset :", test.verset2)
-
 class bcolor:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -84,47 +86,49 @@ class bcolor:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def parter(book, ver1, ver2):
-	"""
+    """
 		Function to print bible verset From file
 		Matio 24:14
 		:param book: 'Matio'
 		:param ver1: '24'
 		:param ver2: '14'
 	"""
-	nbr = 0
-	while nbr < 66:
-		if book == bookList[nbr][0]:
-			filename = bookList[nbr][1]
-			break
-		nbr += 1
+    nbr = 0
+    while nbr < 66:
+        if book == bookList[nbr][0]:
+            filename = bookList[nbr][1]
+            break
 
-	#open file
-	filepath = "/data/data/com.termux/files/home/storage/shared/FloatingBible/bi12_MG/OEBPS/"
-	try:
-		if int(ver1) == 1:
-			filexht = open(filepath + filename + ".xhtml", "r")
-		else:
-			filexht = open(filepath + filename + "-split" + ver1 + ".xhtml", "r")
-	except:
-		print(bcolor.FAIL + "File not found" + bcolor.ENDC)
-		print(bcolor.FAIL + "Invalid chapter: " + bcolor.ENDC + ver1)
-		return False
+        nbr += 1
+    # open file
+    filepath = "/data/data/com.termux/files/home/storage/shared/FloatingBible/bi12_MG/OEBPS/"
+    try:
+        if int(ver1) == 1:
+            filexht = open(filepath + filename + ".xhtml", "r")
+        else:
+            filexht = open(filepath + filename + "-split" + ver1 + ".xhtml", "r")
+    except:
+        print(bcolor.FAIL + "File not found" + bcolor.ENDC)
+        print(bcolor.FAIL + "Invalid chapter: " + bcolor.ENDC + ver1)
+        return False
 
-	#parse file
-	soup = BeautifulSoup(filexht, "html.parser")
-	print(book.capitalize())
-	print(bcolor.OKGREEN + "[" + ver1 + "]" + bcolor.ENDC, ver2)
+    # parse file
+    soup = BeautifulSoup(filexht, "html.parser")
+    print(book.capitalize())
+    print(bcolor.OKGREEN + "[" + ver1 + "]" + bcolor.ENDC, ver2)
+    # for multiples verset
+    for sver2 in ver2:
+        sver2 = str(sver2)
+        vrs = u"chapter" + ver1 + "_verse" + sver2
+        try:
+            stringPart = soup.find("span", attrs={"id": vrs}).next.next.next.next
+        except:
+            print(bcolor.FAIL + "Invalid verset: " + bcolor.ENDC, sver2)
+            break
 
-	#for multiples verset
-	for sver2 in ver2:
-		sver2 = str(sver2)
-		vrs = u"chapter"+ver1+"_verse"+sver2
-
-		try:
-			stringPart = soup.find("span", attrs={"id":vrs}).next.next.next.next
-		except:
-			print(bcolor.FAIL + "Invalid verset: " + bcolor.ENDC, sver2)
-			break
-
-		print(bcolor.OKGREEN + " | " + bcolor.OKBLUE + "["+sver2+"]" + bcolor.ENDC, stringPart)
+        print(
+            bcolor.OKGREEN + " | " + bcolor.OKBLUE + "[" + sver2 + "]" + bcolor.ENDC,
+            stringPart,
+        )
